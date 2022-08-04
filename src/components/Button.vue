2cf8dbb8807a7599b9b640510d91164da29bfeb1
A U M Tuhin <a.tuhin@greyrook.com>
2022-08-04 14:37:48 +0200

    separated movie card into component

---
+++ src/components/Button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button className="btn-default">{{ name }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: Object,
+  },
+  name: "Button",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn-default {
+  width: 5rem;
+  height: 3.6rem;
+  font-size: 1rem;
+  border-radius: 0.3rem;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  border: none;
+  outline: none;
+  background-color: #aab3f9;
+  color: #fff;
+  cursor: pointer;
+  -webkit-transition: 0.3s;
+  -moz-transition: 0.3s;
+  -o-transition: 0.3s;
+  -ms-transition: 0.3s;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: #969ee3;
+  }
+}
+</style>
--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,48 +1,63 @@
 <template>
-  <div class="movies-section">
-    <div v-if="!isLoading">Total: {{ movies.length }}</div>
-    <div class="movies-container">
-      <MovieCard v-for="(movie, index) in movies" :key="index" :movie="movie" />
+  <div class="movie-card">
+    <h4 v-if="movie.name !== null" class="title">{{ movie.name }}</h4>
+    <img
+      v-if="movie.image !== null"
+      class="movie-thumb"
+      :src="movie.image.medium"
+      alt="thumb"
+    />
+    <img
+      v-else
+      class="movie-thumb"
+      src="https://assets.nflxext.com/us/boxshots/hd1080/81034936.jpg"
+      alt="thumb"
+    />
+    <div v-if="movie.rating.average !== null" class="rating">
+      Rating: {{ movie.rating.average }}
     </div>
+    <div v-else class="rating">Rating: No rating</div>
+    <div v-if="movie.genres !== null" class="genres">
+      <p
+        class="genre"
+        v-for="(genre, index) in movie.genres.slice(0, 2)"
+        :key="index"
+      >
+        {{ genre }}
+      </p>
+    </div>
+    <div v-if="movie.language !== null">Language: {{ movie.language }}</div>
+    <div v-if="movie.premiered !== null">Premiered: {movie.premiered}</div>
+    <p v-if="movie.summary !== null" class="summary">
+      {{ movie.summary.replace(regex, "").slice(0, 100) }} ...
+    </p>
+    <a
+      v-if="movie.officialSite !== null"
+      :href="movie.officialSite"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Official Site
+    </a>
+    <p v-else :href="movie.officialSite">No Official Site</p>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import { rootUrl } from "../constants/const";
-import MovieCard from "./MovieCard.vue";
-
 export default {
-  name: "Movies",
-  components: {
-    MovieCard,
+  props: {
+    movie: Object,
   },
+  name: "MovieCard",
   data() {
     return {
-      isLoading: false,
-      movies: [],
+      regex: /(<([^>]+)>)/gi,
     };
-  },
-
-  async mounted() {
-    this.isLoading = true;
-    const response = await axios.get(`${rootUrl}/shows`);
-    if (response.status === 200) {
-      this.movies = response.data.slice(0, 12);
-    }
-    this.isLoading = false;
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.movies-container {
-  padding: 0 2% 2% 2%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
 .movie-card {
   width: 22%;
   margin: 5px;
