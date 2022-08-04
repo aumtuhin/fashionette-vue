f068fca9b3d1a8f37c0a51a8289ddaf8750a6f4a
A U M Tuhin <a.tuhin@greyrook.com>
2022-08-04 14:07:10 +0200

    added movie detail on cards

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,9 +1,42 @@
 <template>
   <div class="movies-section">
-    <div v-if="isLoading">Total: {{ movies.length }}</div>
+    <div v-if="!isLoading">Total: {{ movies.length }}</div>
     <div class="movies-container">
       <div v-for="(movie, index) in movies" :key="index" class="movie-card">
-        <h4 class="title">{{ movie.name }}</h4>
+        <h4 v-if="movie.name !== null" class="title">{{ movie.name }}</h4>
+        <img
+          v-if="movie.image !== null"
+          class="movie-thumb"
+          :src="movie.image.medium"
+          alt="thumb"
+        />
+        <img
+          v-else
+          class="movie-thumb"
+          src="https://assets.nflxext.com/us/boxshots/hd1080/81034936.jpg"
+          alt="thumb"
+        />
+        <div v-if="movie.rating.average !== null" class="rating">
+          Rating: {{ movie.rating.average }}
+        </div>
+        <div v-else class="rating">Rating: No rating</div>
+        <div v-if="movie.genres !== null" class="genres">
+          <p
+            class="genre"
+            v-for="(genre, index) in movie.genres.slice(0, 2)"
+            :key="index"
+          >
+            {{ genre }}
+          </p>
+        </div>
+        <div v-if="movie.language !== null">Language: {{ movie.language }}</div>
+        <div v-if="movie.premiered !== null">Premiered: {movie.premiered}</div>
+        <p v-if="movie.summary !== null" class="summary">
+          {{ movie.summary.replace(regex, "").slice(0, 100) }} ...
+        </p>
+        <a :href="movie.officialSite" target="_blank" rel="noreferrer">
+          <span class="icon">&and;</span>
+        </a>
       </div>
     </div>
   </div>
@@ -16,6 +49,7 @@ export default {
   name: "Movies",
   data() {
     return {
+      regex: /(<([^>]+)>)/gi,
       isLoading: false,
       movies: [],
     };
@@ -102,12 +136,16 @@ export default {
     font-size: 1rem;
   }
 
+  a {
+    text-decoration: none;
+  }
+
   .icon {
-    height: 2rem;
+    font-size: 2rem;
     color: grey;
     border: 1px solid #d8dcfd;
     background-color: #d8dcfd;
-    border-radius: 100%;
+    border-radius: px;
     cursor: pointer;
     outline: none;
 
