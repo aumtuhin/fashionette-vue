3a0562b047f9274c0dacc1c93ef2a6d91337cb78
A U M Tuhin <a.tuhin@greyrook.com>
2022-08-04 12:21:46 +0200

    initialised repo, cleaned up, and added some basic vies and compnents

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Movies from '../components/Movies.vue';
+</script>
+
+<template>
+  <div>
+    <Movies msg='Helloo '/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
